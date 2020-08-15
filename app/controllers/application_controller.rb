f9afaa4bd0159fa6d15e1f195b1a93898084d156
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-15 09:24:10 -0700

    changes text input form to a textarea form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,10 @@ class ApplicationController < Sinatra::Base
         def current_user
             @current_user ||= User.find_by(:email => session[:email]) if session[:email]
         end
+
+        def logout!
+            session.clear
+        end
         
     end
     
