50f40c52874d2615e2fd469719f06677903f0fc0
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-14 22:00:11 -0700

    updates views and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,31 @@
 class PostsController < ApplicationController
 
     get '/' do
-        redirect "/posts"
+        erb :index
     end
 
     get '/posts' do
         @posts = Post.all
-        erb :index
+        erb :"posts/index"
+    end
+
+    get '/posts/new' do
+        if logged_in?
+            erb :"posts/new"
+        else 
+            redirect "/login"
+        end
+    end
+
+    post '/posts' do 
+        post = Post.new(params[:post])
+        post.save
+        redirect "/posts/#{post.id}"
+    end
+
+    get '/posts/:id' do 
+        @post = Post.find_by_id(params[:id])
+        erb :"posts/show"
     end
 
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
-class UsersController > ApplicationController
+class UsersController < ApplicationController
     
     get '/signup' do
         erb :"users/new"
     end 
 
     post '/users' do 
-        @user = User.new(params)
+        @user = User.new(params[:user])
         if @user.save
+            redirect "/login"
+        else 
+            erb :"users/new"
+        end
+    end
+
+
 
 end
